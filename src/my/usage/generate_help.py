234ab5a7790ffd4e4b7b21ec9fa62a806899d085
Peter Reutemann <fracpete@gmail.com>
2023-10-05 09:28:46 +1300

    tweaked

--- src/my/usage/generate_help.py
+++ src/my/usage/generate_help.py
@@ -1,6 +1,6 @@
 from my.registry import REGISTRY, ENTRYPOINT_MYPLUGINS
-from seppl import Plugin, output_help, HELP_FORMAT_MARKDOWN
+from seppl import Plugin, generate_help, HELP_FORMAT_MARKDOWN
 
 plugins = REGISTRY.plugins(ENTRYPOINT_MYPLUGINS, Plugin)
 # this will generate markdown files for each of the plugins in the current directory
-output_help(plugins.values(), help_format=HELP_FORMAT_MARKDOWN, output=".")
+generate_help(plugins.values(), help_format=HELP_FORMAT_MARKDOWN, output_path=".")
